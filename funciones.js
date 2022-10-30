1f3fd9747c85e7dfe27ab029b52ec0c615fc77a7
unknown <ing.raul_vergara@hotmail.com>
2022-10-30 03:55:34 -0600

    En esta fase ya muestra la tabla

--- funciones.js
+++ funciones.js
@@ -5,3 +5,10 @@ function doGet(){
 function obtenerDatosHTML(nombre){
     return HtmlService.createHtmlOutputFromFile(nombre).getContent();
 }
+
+function obtenerRegActividades(){
+    let hoja = SpreadsheetApp.openById('1QqwnHeV1tSnsCgQtiE4qH255UAqcSlbZLM5yFTQXjzI');
+    let bdHoja = hoja.getSheetByName("Inventario-Actvidades");
+    let datos = bdHoja.getDataRange().getDisplayValues();
+    return datos;
+}
